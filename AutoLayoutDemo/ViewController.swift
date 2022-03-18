469a3c52cb86c3645a3fa5a747f22c123c0b87be
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2022-03-18 22:45:07 +0300

    Initial Commit

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Ацамаз Бицоев on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
